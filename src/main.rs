e9a85638de358ff61769826cc4420baeea927f0e
Kevin Conley <koins@duck.com>
2022-12-23 07:50:41 -0800

    how about this? part deux

--- src/main.rs
+++ src/main.rs
@@ -81,10 +81,10 @@ fn discovery(args_bundle: &Arc<imd::DiscoveryArgs>) -> Result<(), Box<dyn Error>
     }));
 
     // Scan common TCP ports and perform service discovery
-    let port_scan = match ports::common_tcp_ports(&args_bundle.clone()) {
-        Ok(port_scan) => port_scan,
-        Err(_) => return Err("Common TCP port scan".into()),
-    };
+    //let port_scan = match ports::common_tcp_ports(&args_bundle.clone()) {
+    //    Ok(port_scan) => port_scan,
+    //    Err(_) => return Err("Common TCP port scan".into()),
+    //};
 
     let port_scan = thread::spawn({
         let args_bundle = args_bundle.clone();
